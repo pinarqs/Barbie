8d204348d986d083a0e89c0bbc3932e8e47f1326
pinarqs <atilirpinar272@gmail.com>
2026-03-08 21:24:29 +0300

    Add laser element to startGame function

--- heist.js
+++ heist.js
@@ -1,83 +1,10 @@
-/* ELMASTA PARILTILI PATLAMA */
-
-function sparkleExplosion(x,y){
-
-for(let i=0;i<6;i++){
-
-let s=document.createElement("div")
-
-s.className="magicSparkle"
-
-s.innerText="✨"
-
-s.style.left=x+"px"
-s.style.top=y+"px"
-
-document.getElementById("gameArea").appendChild(s)
-
-setTimeout(()=>{
-
-s.remove()
-
-},1000)
-
-}
-
-}
-
-
-/* MEVCUT ELMASTA EKLE */
-
-document.addEventListener("click",(e)=>{
-
-sparkleExplosion(e.clientX,e.clientY)
-
-})
-
-
-/* BÜYÜLÜ GÖKKUŞAĞI PARILTISI */
-
-function rainbowGlow(){
-
-document.body.style.boxShadow="0 0 60px pink"
-
-setTimeout(()=>{
-
-document.body.style.boxShadow="none"
-
-},1000)
-
-}
-
-setInterval(rainbowGlow,5000)
-/* GÖKKUŞAĞI EFEKTİ */
-
-function rainbowEffect(){
-
-let rainbow=document.createElement("div")
-
-rainbow.className="rainbowMagic"
-
-document.body.appendChild(rainbow)
-
-setTimeout(()=>{
-
-rainbow.remove()
-
-},4000)
-
-}
-
-/* 10 saniyede bir gökkuşağı */
-
-setInterval(rainbowEffect,10000)
-let score=0
-
 function startGame(){
 
 let area=document.getElementById("gameArea")
 
 area.innerHTML=""
+
+/* ELMASTAR */
 
 for(let i=0;i<5;i++){
 
@@ -106,37 +33,11 @@ area.appendChild(diamond)
 
 }
 
-}
+/* LAZER */
 
-function secondCode(){
+let laser=document.createElement("div")
+laser.className="laser"
 
-alert("🔐 Gizli kasa henüz aktif değil!")
-
-}
-
-function safeGame(){
-
-let guess=prompt("1-5 arası sayı tahmin et")
-
-let num=Math.floor(Math.random()*5)+1
-
-if(guess==num){
-
-alert("🎉 Kazandın!")
+area.appendChild(laser)
 
 }
-
-else{
-
-alert("❌ Kaybettin")
-
-}
-
-}
-
-function finishHeist(){
-
-alert("🏆 Heist tamamlandı! Toplam puan: "+score)
-
-}
-
